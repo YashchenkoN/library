dc88ced5dd91d5e7ee8d90344ab9979a31f2904f
kolyan <vkont4@gmail.com>
2016-03-14 17:54:11 +0200

    add favorite entity and services

--- src/main/java/test/task/library/dao/FavoriteDao.java
+++ src/main/java/test/task/library/dao/FavoriteDao.java
@@ -1,7 +1,16 @@
 package test.task.library.dao;
 
+import test.task.library.entity.Favorite;
+import test.task.library.entity.User;
+
 /**
  * @author Nikolay Yashchenko
  */
-public class FavoriteDao {
+public interface FavoriteDao {
+    Favorite create(Favorite favorite);
+    Favorite read(Long id);
+    Favorite readByUser(User user);
+    Favorite readByUserId(Long id);
+    Favorite update(Favorite favorite);
+    void delete(Favorite favorite);
 }
